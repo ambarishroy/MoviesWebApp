ac0e843a9262098c5c4db525a142e7ee95fa5756
Ambarish Roy <ambarishroy1996@gmail.com>
2025-04-10 22:16:12 +0100

    Refactor: Favourite movie list now stored in a context

--- moviesApp/src/components/movieCard/index.tsx
+++ moviesApp/src/components/movieCard/index.tsx
@@ -14,8 +14,9 @@ import IconButton from "@mui/material/IconButton";
 import img from '../../images/film-poster-placeholder.png';
 import { BaseMovieProps } from "../../types/interfaces"; 
 import { Link } from "react-router-dom";
-import React, {MouseEvent} from "react";
+import React, {MouseEvent, useContext} from "react";
 import Avatar from "@mui/material/Avatar";
+import { MoviesContext } from "../../contexts/moviesContext";
 
 
 const styles = {
@@ -27,30 +28,32 @@ const styles = {
 };
 interface MovieCardProps  {
   movie: BaseMovieProps;
-  selectFavourite: (movieId: number) => void;
 }
-const MovieCard: React.FC<MovieCardProps> = ({movie, selectFavourite}) => {
+const MovieCard: React.FC<MovieCardProps> = ({movie}) => {
+  const { favourites, addToFavourites } = useContext(MoviesContext);//NEW
+
+const isFavourite = favourites.find((id) => id === movie.id)? true : false;//NEW
  
-  const handleAddToFavourite = (e: MouseEvent<HTMLButtonElement>) => {
+  const handleAddToFavourite = (e: MouseEvent<HTMLButtonElement>) => {//NEW
     e.preventDefault();
-    selectFavourite(movie.id);
+    addToFavourites(movie);
   };
   return (
     <Card sx={styles.card}>
-            <CardHeader
-        avatar={
-          movie.favourite ? (
-            <Avatar sx={styles.avatar}>
-              <FavoriteIcon />
-            </Avatar>
-          ) : null
-        }
-        title={
-          <Typography variant="h5" component="p">
-            {movie.title}{" "}
-          </Typography>
-        }
-      />
+    <CardHeader
+      avatar={
+        isFavourite ? (   //CHANGED
+          <Avatar sx={styles.avatar}>
+            <FavoriteIcon />
+          </Avatar>
+        ) : null
+      }
+      title={
+        <Typography variant="h5" component="p">
+          {movie.title}{" "}
+        </Typography>
+      }
+    />
       <CardMedia
         sx={styles.media}
         image={
